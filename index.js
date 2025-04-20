beb251a49acae5b08f9327f3d1c671f701a1956e
Yusuf Badr <yusufbadr@yahoo.com>
2025-04-20 17:20:56 +0200

    changed port number

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
